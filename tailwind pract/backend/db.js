59869b827f953ca4d881432d33873da00ed212b5
Yogeshkodes <yogesh.cr712@gmail.com>
2024-04-04 14:57:01 +0530

    account creation

--- tailwind pract/backend/db.js
+++ tailwind pract/backend/db.js
@@ -5,25 +5,52 @@ mongoose.connect(
   "// mongodb+srv://Yogeshkodes:%40RONALDo%4007%40@cluster0.axgl13t.mongodb.net/"
 );
 
-const Usertable = new mongoose.Schema({
+// Create a Schema for Users
+const userSchema = new mongoose.Schema({
   username: {
-    type: String,
-    required: true,
-  },
-  firstname: {
-    type: String,
-    required: true,
-  },
-  lastname: {
-    type: String,
-    required: true,
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      minLength: 3,
+      maxLength: 30
   },
   password: {
-    type: String,
-    required: true,
+      type: String,
+      required: true,
+      minLength: 6
   },
+  firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 50
+  },
+  lastName: {
+      type: String,
+      required: true,
+      trim: true,
+      maxLength: 50
+  }
 });
 
-const User = mongoose.model("User", Usertable);
+const accountSchema = new mongoose.Schema({
+  userId: {
+      type: mongoose.Schema.Types.ObjectId, // Reference to User model
+      ref: 'User',
+      required: true
+  },
+  balance: {
+      type: Number,
+      required: true
+  }
+});
 
-module.exports = User;
+const Account = mongoose.model('Account', accountSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+User,
+  Account
+};
