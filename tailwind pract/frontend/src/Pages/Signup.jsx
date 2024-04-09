c853bd39791978762551f17531e5f1fcdfb343f4
Yogeshkodes <yogesh.cr712@gmail.com>
2024-04-09 17:26:05 +0530

    updated

--- tailwind pract/frontend/src/Pages/Signup.jsx
+++ tailwind pract/frontend/src/Pages/Signup.jsx
@@ -3,20 +3,58 @@ import { SubHeading } from "../components/SubHeading";
 import { InputBox } from "../components/InputBox";
 import { Button } from "../components/Button";
 import { BottomWarning } from "../components/BottomWarning";
+import { useState } from "react";
+import axios from "axios";
 
 function Signup() {
+  const [Firstname, setFistname] = useState("");
+  const [Lastname, setLastname] = useState("");
+  const [Username, setUsername] = useState("");
+  const [Password, setPassword] = useState("");
+
+  //   console.log(Firstname);
+  //   console.log(Lastname);
+  //   console.log(Username);
+  //   console.log(Password);
+
   return (
     <main className="bg-slate-300 h-screen flex justify-center items-center">
       <div className="h-max w-80  bg-white text-center rounded-lg p-2 px-4">
         <Heading label={"Sign Up"} />
         <SubHeading label={"Enter your information to create your account"} />
 
-        <InputBox placeholder="John" label={"First name"} />
-        <InputBox placeholder="Doe" label={"Last name"} />
-        <InputBox placeholder="Yogesh@gmail.com" label={"Email"} />
-        <InputBox placeholder="123456" label={"Password"} />
+        <InputBox
+          onchange={(e) => setFistname(e.target.value)}
+          placeholder="John"
+          label={"First name"}
+        />
+        <InputBox
+          onchange={(e) => setLastname(e.target.value)}
+          placeholder="Doe"
+          label={"Last name"}
+        />
+        <InputBox
+          onchange={(e) => setUsername(e.target.value)}
+          placeholder="Yogesh@gmail.com"
+          label={"Email"}
+        />
+        <InputBox
+          onchange={(e) => setPassword(e.target.value)}
+          placeholder="123456"
+          label={"Password"}
+        />
         <div className="pt-4">
-          <Button label={"Sign up"} />
+          <Button
+            label={"Sign up"}
+            onClick={() => {
+              axios.post("https://localhost:3000/api/v1/user/signup", {
+                username: Username,
+                password: Password,
+                firstName: Firstname,
+                lastName: Lastname,
+              });
+            }}
+          />
         </div>
         <BottomWarning
           label={"Already have an account ? "}
