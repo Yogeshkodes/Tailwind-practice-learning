5696fe3d4b2ffb7616c9781f3a15860bfd2d7e0d
Yogeshkodes <yogesh.cr712@gmail.com>
2024-04-09 09:59:51 +0530

    created router and PAges

--- tailwind pract/frontend/src/App.jsx
+++ tailwind pract/frontend/src/App.jsx
@@ -1,5 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Signin from "./Pages/Signin";
+import SendMoney from "./Pages/SendMoney";
+import Signup from "./Pages/Signup";
+import Dashboard from "./Pages/Dashboard";
+
+
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
